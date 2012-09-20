563bb4a0a282883f839e62319ff0065a1d584d48
A Flores <ctrl4ltdeleteme@gmail.com>
2012-09-20 15:51:14 +0200

    HADOKEN!

---
+++ admin/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Admin::Application.config.secret_token = '65f0f33b1b0e10dea4bb8f574af14b64cb5d9f3d4f91c907dc87e6f1d77768043e5cfc69701e45fa215c8ed1ee3a23ee5ad6eacf535d46cbffef0581e5e1ca38'
---
+++ frontend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frontend::Application.config.secret_token = 'fefdaf5d546958f9db507ec384bc6091e89072f0968061140e3b193c788e32c2729404bee198d1249f693b775460f17149b0529377eea9503f954358354c0077'
